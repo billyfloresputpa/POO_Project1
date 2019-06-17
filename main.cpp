7cd759ef2025f08adf9aa4a1c3d0526f4ea8a7b7
billyfloresputpa <51890804+billyfloresputpa@users.noreply.github.com>
2019-06-17 09:33:41 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,28 +6,37 @@ using namespace std;
 int main() {
     int num, nivel_col;
     num = Menu();
-    int **matrix;
+    int **matrix,cont;
     switch (num) {
         case 1:
             nivel_col = 7;
             matrix = MatrizInternaIngles(nivel_col);
             Imprimir(matrix,nivel_col);
-            MoverPieza(matrix);
-            Imprimir(matrix,nivel_col);
+            do{
+                MoverPieza(matrix);
+                Imprimir(matrix,nivel_col);
+                cont = ComprobarMatrix(matrix,nivel_col);
+            }while(cont>1);
             break;
         case 2:
             nivel_col = 9;
             matrix = MatrizInternaAleman(nivel_col);
             Imprimir(matrix,nivel_col);
-            MoverPieza(matrix);
-            Imprimir(matrix,nivel_col);
+            do{
+                MoverPieza(matrix);
+                Imprimir(matrix,nivel_col);
+                cont = ComprobarMatrix(matrix,nivel_col);
+            }while(cont>1);
             break;
         case 3:
             nivel_col = 7;
             matrix = MatrizInternaFrances(nivel_col);
             Imprimir(matrix,nivel_col);
-            MoverPieza(matrix);
-            Imprimir(matrix, nivel_col);
+           do{
+                MoverPieza(matrix);
+                Imprimir(matrix,nivel_col);
+                cont = ComprobarMatrix(matrix,nivel_col);
+            }while(cont>1);
             break;
 
     }
