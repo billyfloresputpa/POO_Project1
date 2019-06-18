1999679724931d02b3966c2b1146d5ea658c713e
billyfloresputpa <51890804+billyfloresputpa@users.noreply.github.com>
2019-06-18 11:58:45 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,48 +4,54 @@
 using namespace std;
 
 int main() {
-    int num, nivel_col;
+    int num, nivel_col,cont,cont1;
     num = Menu();
-    int **matrix,cont,cont1;
+    int **matrix;
     switch (num) {
         case 1:
             nivel_col = 7;
             matrix = MatrizInternaIngles(nivel_col);
             Imprimir(matrix,nivel_col);
             do{
-            do{
                 MoverPieza(matrix);
                 Imprimir(matrix,nivel_col);
                 cont=ComprobarMatrix(matrix,nivel_col);
-            }while(cont>1);
                 cont1=ComprobarMatrixPerder(matrix,nivel_col);
-            }while(cont1>0);
+            }while((cont>1)||(cont1>0));
+            if(cont==1)
+                cout<<"GANASTE!!";
+            if(cont1!=1)
+                cout<<"PERDISTE!!";
             break;
         case 2:
             nivel_col = 9;
             matrix = MatrizInternaAleman(nivel_col);
             Imprimir(matrix,nivel_col);
             do{
-                do{
-                    MoverPieza(matrix);
-                    Imprimir(matrix,nivel_col);
-                    cont=ComprobarMatrix(matrix,nivel_col);
-                }while(cont>1);
+                MoverPieza(matrix);
+                Imprimir(matrix,nivel_col);
+                cont=ComprobarMatrix(matrix,nivel_col);
                 cont1=ComprobarMatrixPerder(matrix,nivel_col);
-            }while(cont1>0);
+            }while((cont>1)||(cont1>0));
+            if(cont==1)
+                cout<<"GANASTE!!";
+            if(cont1!=1)
+                cout<<"PERDISTE!!";
             break;
         case 3:
             nivel_col = 7;
             matrix = MatrizInternaFrances(nivel_col);
             Imprimir(matrix,nivel_col);
             do{
-                do{
-                    MoverPieza(matrix);
-                    Imprimir(matrix,nivel_col);
-                    cont=ComprobarMatrix(matrix,nivel_col);
-                }while(cont>1);
+                MoverPieza(matrix);
+                Imprimir(matrix,nivel_col);
+                cont=ComprobarMatrix(matrix,nivel_col);
                 cont1=ComprobarMatrixPerder(matrix,nivel_col);
-            }while(cont1>0);
+            }while((cont>1)||(cont1>0));
+            if(cont==1)
+                cout<<"GANASTE!!";
+            if(cont1!=1)
+                cout<<"PERDISTE!!";
             break;
     }
 return 0;
